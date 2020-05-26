0d676a7a4c1b52e4a1c3af39e48b7d9702d7d769
hugoberpar <hugo.ber.par@gmail.com>
2020-05-26 10:12:00 +0200

    PHPRefactotizacionV02

---
+++ DummyTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Tests;
+
+//class DummyTest extends \PHPUnit\Framework\TestCase{
+class DummyTest extends TestCase{
+    
+    
+    /** @test */
+    function test_it_asserts_true(){
+        $this->assertTrue(true);
+    }
+}
---
+++ TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Tests;
+/**
+ * Description of TestCase
+ *
+ * @author hugoberpar
+ */
+use PHPUnit\Framework\TestCase as BaseTestcase;
+
+class TestCase extends BaseTestcase
+{
+    //put your code here
+}
